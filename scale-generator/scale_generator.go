d2c6f0add154d1006b707a09fad5d066c97f6acc
David Berry <david@davidberry.io>
2020-05-19 21:19:58 -0700

    fix for two-fer exercise

--- scale-generator/scale_generator.go
+++ scale-generator/scale_generator.go
@@ -1,1 +1,35 @@
 package scale
+
+var (
+	SharpPitches = [12]string{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"}
+	FlatPitches = [12]string{"C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"}
+	noSharpOrFlatPitches = [12]string{"C", "D", "E", "F", "G", "A", "B"}
+
+	sharpTonics = []string{"G", "D", "A", "E", "B", "F# major", "e", "b", "f#", "c#", "g#", "d# minor"}
+	flatTonics = []string{"F", "Bb", "Eb", "Ab", "Db", "Gb major", "d", "g", "c", "f", "bb", "eb minor"}
+	noSharpOrFlatTonics = []string{"C major", "a minor"}
+)
+
+type (
+)
+
+func getNextTone(currentTone string, intervalToNextTone string, tonic string) string {
+	var distanceToNextTone int
+	if (intervalToNextTone == "M") {
+		distanceToNextTone = 2
+	}
+
+	if (intervalToNextTone == "m") {
+		distanceToNextTone = 1
+	}
+
+	if (intervalToNextTone == "") {
+		distanceToNextTone = -1
+	}
+
+	return "placeholder"
+}
+
+func Scale(tonic string, interval string) []string {
+	return sharpPitches
+}
--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,32 +1,18 @@
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+//Package twofer implements a solution to the exercism.io prompt with the same name.
 package twofer
 
-// ShareWith should have a comment documenting it.
-/*
-Given a name, return a string with the message:
-One for X, one for me.
+import "fmt"
 
-Where X is the given name.
-
-However, if the name is missing, return the string:
-One for you, one for me.
-
-
-Here are some examples:
-
-|Name    |String to return
-|:-------|:------------------
-|Alice   |One for Alice, one for me.
-|Bob     |One for Bob, one for me.
-|        |One for you, one for me.
-|Zaphod  |One for Zaphod, one for me.
-
+/*ShareWith returns the proper twoFer result based on the name string provided
+1. Given a name, return a string with the message:
+	 One for X, one for me.
+2. If no name is provided, return a string with the message:
+   One for you, one for me.
 */
 func ShareWith(name string) string {
-	if (name == "") {
-		return "One for you, one for me."
+	if name == "" {
+		name = "you"
 	}
 
-	return "One for " + name + ", one for me."
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
