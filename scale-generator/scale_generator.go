8992646b27542767f46246dca27d14eda17afb0d
David Berry <david@davidberry.io>
2020-06-05 17:24:09 -0700

    reflect testing

--- scale-generator/scale_generator.go
+++ scale-generator/scale_generator.go
@@ -1,5 +1,9 @@
 package scale
 
+import (
+	"fmt"
+)
+
 var (
 	sharpPitches         = []string{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"}
 	flatPitches          = []string{"C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"}
@@ -11,6 +15,27 @@ var (
 )
 
 type ()
+
+// Contains tells whether a contains x.
+func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+// Find returns the smallest index i at which x == a[i],
+// or len(a) if there is no such index.
+func Find(a []string, x string) int {
+	for i, n := range a {
+		if x == n {
+			return i
+		}
+	}
+	return len(a)
+}
 
 func getNextTone(currentTone string, intervalToNextTone string, tonic string) string {
 	var distanceToNextTone int
@@ -32,5 +57,21 @@ func getNextTone(currentTone string, intervalToNextTone string, tonic string) st
 
 //Scale returns the appropriate scale for the given tonic and interval
 func Scale(tonic string, interval string) []string {
-	return sharpPitches
+	resultScale := make([]string, len(sharpPitches))
+	fmt.Println(resultScale)
+	if tonic == "C" {
+		return sharpPitches
+	}
+
+	fmt.Println(reflect.typeof(resultScale[5]))
+
+	if Contains(flatTonics, tonic) {
+		copy(resultScale, flatPitches)
+		start := Find(resultScale, tonic)
+		fmt.Println(reflect.typeof(resultScale[start]))
+		return reflect.typeof(resultScale[start:])
+		// resultScale = append(resultScale[start:], resultScale[:start])
+	}
+
+	return resultScale
 }
