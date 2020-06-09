a51cab03596e816394cd8974cda6c533eed303fa
david <david@davidberry.io>
2020-06-08 21:29:39 -0700

    leap year initialization

---
+++ leap/leap.go
@@ -0,0 +1,15 @@
+// Package leap implements a solution the exercism.io problem Leap Year
+package leap
+
+/* IsLeapYear returns a boolean determining if the given integer year is a leap year via the following rules:
+on every year that is evenly divisible by 4
+	except every year that is evenly divisible by 100
+		unless the year is also evenly divisible by 400
+*/
+func IsLeapYear(int) bool {
+	// Write some code here to pass the test suite.
+	// Then remove all the stock comments.
+	// They're here to help you get started but they only clutter a finished solution.
+	// If you leave them in, reviewers may protest!
+	return false
+}
