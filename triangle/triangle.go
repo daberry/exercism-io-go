35390f35fb4b0f5c7c939961fb771248f044a748
david <david@davidberry.io>
2020-06-09 12:55:14 -0700

    initial solution to triangle

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -1,28 +1,36 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package triangle should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package triangle implements the exercism.io prompt by the same name
 package triangle
 
+import "math"
 
-// Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+//Kind is a datatype that describes the type of triangle
+type Kind string
 
+//Constants to map triangle types
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	NaT = "NaT"
+	Equ = "Equ"
+	Iso = "Iso"
+	Sca = "Sca"
 )
 
 // KindFromSides should have a comment documenting it.
-func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	var k Kind
-	return k
+func KindFromSides(a, b, c float64) (k Kind) {
+	if a <= 0 || b <= 0 || c <= 0 {
+		k = NaT
+	} else if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) {
+		k = NaT
+	} else if math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) {
+		k = NaT
+	} else if a+b < c || a+c < b || b+c < a {
+		k = NaT
+	} else if a == b && b == c {
+		k = Equ
+	} else if a == b || a == c || b == c {
+		k = Iso
+	} else {
+		k = Sca
+	}
+
+	return
 }
