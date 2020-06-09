ec01b10d2cc27db67ed5b071c9339f22948692a4
david <david@davidberry.io>
2020-06-09 12:28:14 -0700

    accumulate solution + cleanup

--- accumulate/accumulate.go
+++ accumulate/accumulate.go
@@ -1,1 +1,19 @@
+//Package accumulate implements a solution to the exercism.io problem with the same name
 package accumulate
+
+//Converter is a type definition for the provided iterator to map input to output
+type Converter func(string) string
+
+/*
+Accumulate applies the provided iterator to the provided collection and
+returns the result of the transformation
+*/
+func Accumulate(strings []string, conv Converter) (result []string) {
+	result = make([]string, len(strings))
+
+	for i, val := range strings {
+		result[i] = conv(val)
+	}
+
+	return
+}
