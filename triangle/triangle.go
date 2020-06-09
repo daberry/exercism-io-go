b6c2540336a6a05b6b3c9e7d918005d1f1485740
david <david@davidberry.io>
2020-06-09 13:07:06 -0700

    triangle prompt cleanup

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -4,25 +4,27 @@ package triangle
 import "math"
 
 //Kind is a datatype that describes the type of triangle
-type Kind string
+type Kind int
 
 //Constants to map triangle types
 const (
-	NaT = "NaT"
-	Equ = "Equ"
-	Iso = "Iso"
-	Sca = "Sca"
+	NaT = iota // not a triangle
+	Equ        // equilateral
+	Iso        // isosceles
+	Sca        // scalene
 )
 
 // KindFromSides should have a comment documenting it.
 func KindFromSides(a, b, c float64) (k Kind) {
-	if a <= 0 || b <= 0 || c <= 0 {
-		k = NaT
-	} else if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) {
-		k = NaT
-	} else if math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) {
-		k = NaT
-	} else if a+b < c || a+c < b || b+c < a {
+	sides := []float64{a, b, c}
+
+	for _, side := range sides {
+		if side <= 0 || math.IsNaN(side) || math.IsInf(side, 0) {
+			return NaT
+		}
+	}
+
+	if a+b < c || a+c < b || b+c < a {
 		k = NaT
 	} else if a == b && b == c {
 		k = Equ
