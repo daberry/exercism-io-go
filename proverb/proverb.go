59bf6664572fb633f24f7218c5a875dbadd88c35
david <david@davidberry.io>
2020-06-09 18:59:39 -0700

    initial solution

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -1,15 +1,17 @@
 // This is a "stub" file.  It's a little start on your solution.
 // It's not a complete solution though; you have to write some code.
 
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package proverb implements a solution to the exercism.io prompt by the same name
 package proverb
 
-// Proverb should have a comment documenting it.
-func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+// Proverb returns an array of assembled strings
+func Proverb(rhyme []string) (assembled []string) {
+	for i, val := range rhyme {
+		if i < len(rhyme)-1 {
+			assembled = append(assembled, "For want of a "+val+" the "+rhyme[i+1]+" was lost.")
+		} else {
+			assembled = append(assembled, "And all for the want of a "+rhyme[0]+".")
+		}
+	}
+	return
 }
