86a57ed365170ae6389f258e78b1f379f16effdb
david <david@davidberry.io>
2020-06-09 19:36:19 -0700

    cleanup of proverb solution

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -4,13 +4,25 @@
 // Package proverb implements a solution to the exercism.io prompt by the same name
 package proverb
 
+import "fmt"
+
+const (
+	stanza = "For want of a %s the %s was lost."
+	last   = "And all for the want of a %s."
+)
+
 // Proverb returns an array of assembled strings
-func Proverb(rhyme []string) (assembled []string) {
+func Proverb(rhyme []string) (assembledProverb []string) {
+	if len(rhyme) == 0 {
+		return
+	}
+
+	assembledProverb = make([]string, len(rhyme))
 	for i, val := range rhyme {
-		if i < len(rhyme)-1 {
-			assembled = append(assembled, "For want of a "+val+" the "+rhyme[i+1]+" was lost.")
+		if i+1 < len(rhyme) {
+			assembledProverb[i] = fmt.Sprintf(stanza, val, rhyme[i+1])
 		} else {
-			assembled = append(assembled, "And all for the want of a "+rhyme[0]+".")
+			assembledProverb[i] = fmt.Sprintf(last, rhyme[0])
 		}
 	}
 	return
