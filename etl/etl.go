504c10b13eed59ef3b27354abfa8bfaf0b16e8ac
David Berry <david@davidberry.io>
2020-06-10 19:29:07 -0700

    etl solution

--- etl/etl.go
+++ etl/etl.go
@@ -1,1 +1,18 @@
+//Package etl implements a solution to the exercism.io prompt by the same name
 package etl
+
+import "strings"
+
+//Transform modifies the scrabble scoring format to fall in line with the new requirements
+func Transform(input map[int][]string) (output map[string]int) {
+	output = make(map[string]int)
+
+	for i, letters := range input {
+		for _, letter := range letters {
+			letter = strings.ToLower(letter)
+			output[letter] = i
+		}
+	}
+
+	return
+}
