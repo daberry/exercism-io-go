a4b1e04a60b5730f37c33d850d8a490966af2ec0
David Berry <david@davidberry.io>
2020-06-10 22:00:23 -0700

    rna-transcription initial solution

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,4 +1,29 @@
+//Package strand implements a solution to the rna-transcription prompt from exercism.io
 package strand
 
-func ToRNA(dna string) string {
+var transcribe = map[string]string{
+	"G": "C",
+	"C": "G",
+	"T": "A",
+	"A": "U",
+}
+
+/*
+ToRNA returns the RNA strand that compliments the input DNA strand using the following conversion rules:
+ DNA -> RNA
+ `G` -> `C`
+ `C` -> `G`
+ `T` -> `A`
+ `A` -> `U`
+*/
+func ToRNA(dna string) (rna string) {
+	if len(dna) == 0 {
+		return
+	}
+
+	for _, nucleotide := range dna {
+		rna += transcribe[string(nucleotide)]
+	}
+
+	return
 }
