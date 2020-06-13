7875c72d35f66fe257e1af28bd3af88f7e148892
David Berry <david@davidberry.io>
2020-06-12 22:20:52 -0700

    bob pseudocode

--- bob/bob.go
+++ bob/bob.go
@@ -1,15 +1,29 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package bob implements a solution to the exercism.io prompt by the same name
 package bob
 
-// Hey should have a comment documenting it.
+/* Hey returns the proper response for Bob's "personality" as defined in the prompt
+Bob answers 'Sure.' if you ask him a question, such as "How are you?".
+
+He answers 'Whoa, chill out!' if you YELL AT HIM (in all capitals).
+
+He answers 'Calm down, I know what I'm doing!' if you yell a question at him.
+
+He says 'Fine. Be that way!' if you address him without actually saying anything.
+
+He answers 'Whatever.' to anything else.
+*/
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
+	//if remark to uppercase == remark then the person is yelling
+	//if punctuation (last character) is a period
+	//answer with 'Whoa, chill out!'
+	//if punctuation is a question mark
+	//answer with 'Calm down, I know what I'm doing!'
+	//if remark is not yelling (else)
+	//if punctuation is a question mark
+	//answer with 'Sure.'
+	//if remark is blank
+	//answer with 'Fine. Be that way!'
+
+	//otherwise answer with 'Whatever'
 	return ""
 }
