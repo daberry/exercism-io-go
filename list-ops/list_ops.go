32c4cdc3c6a1caf7f7a2786cdc8c970e248c9842
David Berry <david@davidberry.io>
2020-06-14 22:40:00 -0700

    start of length implementation

--- list-ops/list_ops.go
+++ list-ops/list_ops.go
@@ -1,1 +1,16 @@
+//Package listops implements the suite of functions to operate on lists from exercism.io prompt by the same  name
 package listops
+
+import "container/list"
+
+//IntList is a list of integers, implemented so that the test suite can function
+type IntList list.List
+
+//Length returns the number of elements contained within the IntList
+func (il IntList) Length(listLength int) {
+	_, ok := il.Front()
+	for ok {
+
+	}
+	return
+}
